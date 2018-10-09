62867c16a4c697c8efd5145b759659a02c8482be
Michael Roose <mjroose@gmail.com>
2018-10-09 23:17:52 +0000

    automatically backed up by learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -17,7 +17,7 @@ class Genre < ActiveRecord::Base
   def all_artist_names
     # return an array of strings containing every musician's name
     self.songs.collect do |song|
-      song.artist unless song.artist == nil
+      song.artist.name unless song.artist.name == nil
     end.uniq
   end
 end
